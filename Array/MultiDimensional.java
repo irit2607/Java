ebd403dc07f81f0ce3bb5d3756b4db9b65d7c0b6
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2021-12-30 10:10:24 +0530

    Create MultiDimensional.java

---
+++ Array/MultiDimensional.java
@@ -0,0 +1,12 @@
+// we have mlti-dimensional array. Write a code to find the length of row 0.
+public class Main
+{
+	public static void main(String[] args) {
+		int[][] multiDimensional = {
+		        {1,2},
+		        {3,4},
+		        {5,6}
+		   };
+		System.out.println("size of first array in multidimensional " + multiDimensional[0].length);
+	}
+}
