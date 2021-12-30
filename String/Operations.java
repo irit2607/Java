caae27b56324ac60200ecf67e91f6c233b4d2b50
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2021-12-30 11:47:40 +0530

    Create Operations.java

---
+++ String/Operations.java
@@ -0,0 +1,10 @@
+public class Operations
+{
+	public static void main(String[] args) {
+	    String s1 = "Happy";
+	    String s2 = "Learning";
+	    System.out.println(s1.compareTo(s2));              // to compare strings
+		System.out.println(s1.length());                         //length of string
+		System.out.println(s1.substring(2));                 
+	}
+}
