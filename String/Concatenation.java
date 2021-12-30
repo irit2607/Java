9034e43c07836117c09584bc384438f85026f582
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2021-12-30 11:40:04 +0530

    Create Concatenation.java

---
+++ String/Concatenation.java
@@ -0,0 +1,10 @@
+
+public class Concatenation
+{
+	public static void main(String[] args) {
+	    String s1 = "Happy";
+		System.out.println("Original String: " +s1);
+		s1 = s1.concat(" Learning");
+		System.out.println("After concatenation: " +s1);
+	}
+}
