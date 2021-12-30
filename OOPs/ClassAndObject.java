e1807d8d56c26ae280e5c33d11dbfc4232d1fa6b
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2021-12-30 21:39:58 +0530

    Create ClassAndObject.java

---
+++ OOPs/ClassAndObject.java
@@ -0,0 +1,13 @@
+class Demo{
+    void printMessage()
+    {
+        System.out.println("Classes & Objects");
+    }
+}
+public class ClassAndObject
+{
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		d.printMessage();
+	}
+}
