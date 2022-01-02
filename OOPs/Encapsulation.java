821c9092dd838c1b71fd0b1c46688656cb7abd77
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2022-01-02 10:30:05 +0530

    Create Encapsulation.java

---
+++ OOPs/Encapsulation.java
@@ -0,0 +1,25 @@
+class Employee{
+    private String name;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+public class Encapsulation
+{
+	public static void main(String[] args) {
+	    Employee e = new Employee();
+	    e.setName("Irit");
+	    System.out.println(e.getName());
+	}
+}
+
+/* o/p
+Irit
+*/
