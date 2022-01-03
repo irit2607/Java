212e601e1671bfd39e1800e9238607f3e6d8163b
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2022-01-03 17:34:25 +0530

    Update InputOutput.java

--- File/InputOutput.java
+++ File/InputOutput.java
@@ -1,17 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class InputOutput
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-	    File employeeInfoFile = new File("E:\\EmployeeInfo.txt");
+	    File employeeInfoFile = new File("E:\\EmployeeInfo.txt");             // file to be write
 	    InputOutput demo = new InputOutput();
 	    demo.writeToFile(employeeInfoFile);
+	    File vehicleInfo = new File("VehicleInfo.txt");                        // file to be read
 	}
 	
-	public void writeToFile(File fileToWrite)
+	public void writeToFile(File fileToWrite)                                  //write
 	{
 	    PrintWriter employeeInfoWrite = null;
 	    try
@@ -29,4 +31,15 @@ public class InputOutput
 	        employeeInfoWrite.close();
 	    }
 	}
+	
+	public void readToFile(File fileToRead) throws FileNotFoundException             //   read
+	{
+	    Scanner input = new Scanner(fileToRead);
+	    while(input.hasNext())
+	    {
+	        System.out.println("Lines read from i/p file :" +input.nextLine());
+	    }
+	}
 }
+
+
