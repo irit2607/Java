54b1b935ee399c34b563fac9d610d50c87eb54f3
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2022-01-06 15:11:04 +0530

    Create Enumerator.java

---
+++ Enumerator.java
@@ -0,0 +1,18 @@
+// enum declaration inside a class.
+public class Enumerator
+{
+    enum Color
+    {
+        RED, GREEN, BLUE;
+    }
+ 
+    // Driver method
+    public static void main(String[] args)
+    {
+        Color c1 = Color.RED;
+        System.out.println(c1);
+    }
+}
+
+//o/p
+//RED
